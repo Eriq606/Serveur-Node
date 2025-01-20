16ae6341bd7699f9888ff17925274cd11ed20808
Eriq606 <erohweltall@gmail.com>
2025-01-20 11:07:29 +0300

    rewrote index.js to use express

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-var server=require("./server");
-var router=require("./router");
-var requestHandlers=require("./requestHandlers");
+var express = require("express")
+var app=express();
 
-var handle={};
-handle["/"]=requestHandlers.start;
-handle["/start"]=requestHandlers.start;
-handle["/upload"]=requestHandlers.upload;
-
-server.start(router.route, handle);
+function start(response){
+    console.log("Le gestionnaire 'start' est appelé.");
+    setTimeout(function(){
+        response.send("Bonjour Start");
+    }, 10000);
+}
+function upload(response){
+    console.log("Le gestionnaire 'upload' est appelé.");
+    response.send("Bonjour Upload - Rafraîchi");
+}
+app.get("/", function(req, res){
+    start(res);
+});
+app.get("/start", function(req, res){
+    start(res);
+});
+app.get("/upload", function(req, res){
+    upload(res);
+});
+app.use(function(req, res){
+    res.setHeader("Content-Type", "text/plain");
+    res.send(404, "Page not found");
+});
+app.listen(3000, function(){
+    console.log("App listening on port 3000!");
+});
